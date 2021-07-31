dd266658257d877acabfa23f70a6f2ab2a6c07c2
hhhhungc <dial012@yahoo.com.tw>
2021-07-31 13:59:00 +0800

    update sum.js

--- basic/sum.js
+++ basic/sum.js
@@ -2,9 +2,12 @@ console.log("Hello World");
 
 function sum(n){
     let result=0;
-    for(let i=0;i<=n;i++){
+    for(let i=1;i<=n;i++){
         result+=i
     }    
     return result;
 }
-console.log(sum(10))
+
+console.log(sum(1));
+console.log(sum(2));
+console.log(sum(10));
