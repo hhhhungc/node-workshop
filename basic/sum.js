9c1aa47d07763852c41c6a154a4b58d5c68f85e3
hhhhungc <dial012@yahoo.com.tw>
2021-08-01 15:02:42 +0800

    xhr的準備工作

--- basic/sum.js
+++ basic/sum.js
@@ -1,13 +1,20 @@
 console.log("Hello World");
 
-function sum(n){
-    let result=0;
-    for(let i=1;i<=n;i++){
-        result+=i
-    }    
+// 1+2+...+n
+// 方法1
+function sum2(n){
+    return (n+1)*n/2
+}
+console.log(sum2(10));
+
+// 方法2
+function sum(n) {
+    let result = 0;
+    for (let i = 1; i <= n; i++) {
+        result += i;
+    }
     return result;
 }
-
 console.log(sum(1));
 console.log(sum(2));
 console.log(sum(10));
