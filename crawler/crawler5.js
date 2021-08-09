878298ee564b5b75488deb494fe0a1e8b7890915
hhhhungc <dial012@yahoo.com.tw>
2021-08-09 19:12:23 +0800

    add crawler5.js (存進資料庫createPool版??)

--- crawler/crawler5.js
+++ crawler/crawler5.js
@@ -10,63 +10,6 @@ const mysql = require("mysql2");
 
 require("dotenv").config();
 
-// const connection = mysql.createConnection({
-// host: process.env.DB_HOST,
-// user: process.env.DB_USER,
-// port: process.env.DB_PORT,
-// password: process.env.DB_PASSWORD,
-// database: process.env.DB_NAME,
-// });
-
-// connection.connect((err) => {
-//     if (err) {
-//         console.error("資料庫連不上");
-//     }
-// });
-
-// function getStockCode() {
-//     return new Promise((resolve, reject) => {
-//         fs.readFile("stock.txt", "utf8", (err, stockCode) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(stockCode);
-//             }
-//         });
-//     });
-// }
-
-// function getQuery(stockCode) {
-//     // return new Promise((resolve, reject) => {
-//     //     connection.query(
-//     //         "SELECT * FROM stock WHERE stock_id =?",
-//     //         [stockCode],
-//     //         function (error, results, fields) {
-//     //             if (error) {
-//     //                 reject(error);
-//     //             }
-//     //             resolve(results);
-//     //             // console.log(results);
-//     //         }
-//     //     );
-//     // });
-// }
-
-// function insertSQL(parseddata) {
-//     return new Promise((resolve, reject) => {
-//         promisePool.query(
-//             "INSERT IGNORE INTO stock_price (stock_id, date, volume, amount, open_price, high_price, low_price, close_price, delta_price, transactions) VALUE ?",
-//             [parseddata],
-//             function (error, results, fields) {
-//                 if (error) {
-//                     reject(error);
-//                 }
-//                 resolve(results);
-//             }
-//         );
-//     });
-// }
-
 function getResponse(stockCode) {
     return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
         params: {
@@ -89,12 +32,10 @@ async function stockDay() {
 
     try {
         // 1. 讀 stock.txt 把股票代碼讀進來
-        // let stockCode = await getStockCode(); // fs改promise版
         let stockCode = await fs.readFile("stock.txt", "utf8");
         // console.log("stockCode :", stockCode);
 
         // 2. 去資料庫的 stock 表格查看看，這個代碼是不是在我們的服務範圍內
-        // let stockQuery = await getQuery(stockCode); // mysql1改mysql2版
         let [rows] = await promisePool.query(
             "SELECT * FROM stock WHERE stock_id =?",
             [stockCode]
@@ -102,7 +43,6 @@ async function stockDay() {
         // console.log("資料庫連線成功", rows);
 
         if (rows.length === 0) {
-            // console.warn("此筆股票資料不在資料庫裡!");
             throw "此筆股票資料不在資料庫裡!";
         }
 
@@ -120,12 +60,10 @@ async function stockDay() {
             item[0] = parseInt(item[0].replace(/\//g, "")) + 19110000;
             item.unshift(stockCode);
             return item;
-            // console.log(item);
         });
         // console.log(parseddata);
 
         // 4. 抓回來的資料存到資料庫的 stock_price 表格裡去
-        // let results = await insertSQL(parseddata);
         let [insertResult] = await promisePool.query(
             "INSERT IGNORE INTO stock_price (stock_id, date, volume, amount, open_price, high_price, low_price, close_price, delta_price, transactions) VALUE ?",
             [parseddata]
